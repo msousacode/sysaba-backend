1cb0babce145deb2ceda4757725e19a7c5b01469
Michael Sousa <msousacode@gmail.com>
2024-11-12 22:34:21 -0300

    first commit

---
+++ src/main/java/br/com/sysaba/SysabaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sysaba/SysabaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
